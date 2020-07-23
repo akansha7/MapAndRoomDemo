2d432d99d547b4d1062b21c19e59d679ef993b55
akansha7 <patelakansha17@gmail.com>
2020-07-23 10:18:43 +0530

    Manage autocomplete places api with search and direct to that place functionality.

--- app/src/main/java/com/app/mapandroom/activity/MainActivity.java
+++ app/src/main/java/com/app/mapandroom/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -40,10 +41,26 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.CustomCap;
+import com.google.android.gms.maps.model.Dash;
+import com.google.android.gms.maps.model.Dot;
+import com.google.android.gms.maps.model.Gap;
+import com.google.android.gms.maps.model.JointType;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PatternItem;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.RoundCap;
+import com.google.android.gms.maps.model.VisibleRegion;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
@@ -58,8 +75,11 @@ import com.google.android.libraries.places.widget.AutocompleteActivity;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+import com.google.maps.android.data.geojson.GeoJsonLayer;
+import com.google.maps.android.data.geojson.GeoJsonPolygonStyle;
 
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -70,7 +90,9 @@ import java.util.Locale;
  * An activity that displays a map showing the place at the device's current location.
  */
 public class MainActivity extends AppCompatActivity
-        implements View.OnClickListener, OnMapReadyCallback {
+        implements View.OnClickListener, OnMapReadyCallback ,
+        GoogleMap.OnPolylineClickListener,
+        GoogleMap.OnPolygonClickListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private GoogleMap map;
@@ -104,6 +126,34 @@ public class MainActivity extends AppCompatActivity
 
     MyAppDatabase myAppDatabase =AppController.getMyAppDatabase();
     private String strAddress = "";
+
+    // [START maps_poly_activity_style_polygon]
+    private static final int COLOR_WHITE_ARGB = 0xffffffff;
+    private static final int COLOR_GREEN_ARGB = 0xff388E3C;
+    private static final int COLOR_PURPLE_ARGB = 0xff81C784;
+    private static final int COLOR_ORANGE_ARGB = 0xffF57F17;
+    private static final int COLOR_BLUE_ARGB = 0xffF9A825;
+
+    private static final int POLYGON_STROKE_WIDTH_PX = 8;
+    private static final int PATTERN_DASH_LENGTH_PX = 20;
+    private static final PatternItem DASH = new Dash(PATTERN_DASH_LENGTH_PX);
+    private static final int PATTERN_GAP_LENGTH_PX = 20;
+    private static final PatternItem DOT = new Dot();
+    private static final PatternItem GAP = new Gap(PATTERN_GAP_LENGTH_PX);
+
+    // Create a stroke pattern of a gap followed by a dot.
+    private static final List<PatternItem> PATTERN_POLYLINE_DOTTED = Arrays.asList(GAP, DOT);
+
+    // [START maps_poly_activity_style_polyline]
+    private static final int COLOR_BLACK_ARGB = 0xff000000;
+    private static final int POLYLINE_STROKE_WIDTH_PX = 12;
+
+    // Create a stroke pattern of a gap followed by a dash.
+    private static final List<PatternItem> PATTERN_POLYGON_ALPHA = Arrays.asList(GAP, DASH);
+
+    // Create a stroke pattern of a dot followed by a gap, a dash, and another gap.
+    private static final List<PatternItem> PATTERN_POLYGON_BETA =
+            Arrays.asList(DOT, GAP, DASH, GAP);
 
     // [START maps_current_place_on_create]
     @Override
@@ -256,15 +306,39 @@ public class MainActivity extends AppCompatActivity
      * Manipulates the map when it's available.
      * This callback is triggered when the map is ready to be used.
      */
+
+    private PolygonOptions mPolygon = new PolygonOptions();
+
     @Override
     public void onMapReady(final GoogleMap map) {
         this.map = map;
 
         map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-
             @Override
             public void onMapClick(LatLng latLng) {
                 redirectMarkerToLatLngPosition(latLng);
+
+
+//                map.getUiSettings().setZoomControlsEnabled(true);
+//
+//                LatLng madrid = new LatLng(23.044371, 72.517921);
+//                map.moveCamera(CameraUpdateFactory.newLatLngZoom(madrid, 3F));
+//
+//                try {
+//                    GeoJsonLayer layer = new GeoJsonLayer(map, R.raw.es_geojson, getApplicationContext());
+//
+//                    GeoJsonPolygonStyle style = layer.getDefaultPolygonStyle();
+//                    style.setFillColor(Color.MAGENTA);
+//                    style.setStrokeColor(Color.MAGENTA);
+//                    style.setStrokeWidth(1F);
+//
+//                    layer.addLayerToMap();
+//
+//                } catch (IOException ex) {
+//                    Log.e("IOException", ex.getLocalizedMessage());
+//                } catch (JSONException ex) {
+//                    Log.e("JSONException", ex.getLocalizedMessage());
+//                }
             }
         });
         // Prompt the user for permission.
@@ -273,6 +347,122 @@ public class MainActivity extends AppCompatActivity
         updateLocationUI();
         // Get the current location of the device and set the position of the map.
         getDeviceLocation();
+
+
+
+        // Add polylines to the map.
+        // Polylines are useful to show a route or some other connection between points.
+        Polyline polyline1 = map.addPolyline(new PolylineOptions()
+                .clickable(true)
+                .add(
+                        new LatLng(23.044371, 72.517921),
+                        new LatLng(23.059970, 72.498160),
+                        new LatLng(23.044708, 72.517426),
+                        new LatLng(23.044313,72.515665),
+                        new LatLng(23.042615,72.517125),
+                        new LatLng(23.042615,72.519787)));
+        stylePolyline(polyline1);
+        polyline1.setTag("A");
+        // [END maps_poly_activity_add_polyline_set_tag]
+        // Style the polyline.
+        stylePolyline(polyline1);
+
+        Polyline polyline2 = map.addPolyline(new PolylineOptions()
+                .clickable(true)
+                .add( new LatLng(23.044371, 72.517921),
+                        new LatLng(23.059970, 72.498160),
+                        new LatLng(23.044708, 72.517426),
+                        new LatLng(23.044313,72.515665),
+                        new LatLng(23.042615,72.517125),
+                        new LatLng(23.042615,72.519787)));
+        polyline2.setTag("B");
+        stylePolyline(polyline2);
+
+        // [START maps_poly_activity_add_polygon]
+        // Add polygons to indicate areas on the map.
+        Polygon polygon1 = map.addPolygon(new PolygonOptions()
+                .clickable(true)
+                .add(
+                        new LatLng(23.053699,72.472229),
+                        new LatLng(23.040246,72.467770),
+                        new LatLng(23.043721,72.488039),
+                        new LatLng(23.059200,72.485291)));
+        // Store a data object with the polygon, used here to indicate an arbitrary type.
+        polygon1.setTag("alpha");
+        // [END maps_poly_activity_add_polygon]
+        // Style the polygon.
+        stylePolygon(polygon1);
+
+//        Polygon polygon2 = map.addPolygon(new PolygonOptions()
+//                .clickable(true)
+//                .add(
+//                        new LatLng(-31.673, 128.892),
+//                        new LatLng(-31.952, 115.857),
+//                        new LatLng(-17.785, 122.258),
+//                        new LatLng(-12.4258, 130.7932)));
+//        polygon2.setTag("beta");
+//        stylePolygon(polygon2);
+        // [END_EXCLUDE]
+
+        // Position the map's camera near Alice Springs in the center of Australia,
+        // and set the zoom factor so most of Australia shows on the screen.
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-23.684, 133.903), 4));
+        // Set listeners for click events.
+        map.setOnPolylineClickListener(this);
+        map.setOnPolygonClickListener(this);
+
+        Circle circle = map.addCircle(new CircleOptions()
+                .center(new LatLng( 23.045616,72.511033))
+                .radius(100)
+                .strokeColor(Color.RED)
+                .fillColor(Color.BLUE));
+
+
+
+//        VisibleRegion vr = map.setLatLngBoundsForCameraTarget(lastKnownLocation.);
+//        double left = vr.latLngBounds.southwest.longitude;
+//        double top = vr.latLngBounds.northeast.latitude;
+//        double right = vr.latLngBounds.northeast.longitude;
+//        double bottom = vr.latLngBounds.southwest.latitude;
+//
+//        Log.e(TAG, "vr=> "+vr);
+//        Log.e(TAG, "left=> "+left);
+//        Log.e(TAG, "top=> "+top);
+//        Log.e(TAG, "right=> "+right);
+//        Log.e(TAG, "bottom=> "+bottom);
+
+
+//        LatLngBounds bounds = map.getProjection().getVisibleRegion().latLngBounds;
+//
+//        Log.e(TAG, "bounds=> "+bounds);
+
+//        if (bounds.contains("Supercity Lifestyle Township")) {
+//            marker = map.addMarker(
+//                    new MarkerOptions()
+//                            .position(ROMA)
+//                            .title("Hello")
+//                            .snippet("Nice Place")
+//                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher))
+//            );
+//            System.out.println("Marker added");
+//        }
+
+        LatLngBounds bounds1 = map.getProjection().getVisibleRegion().latLngBounds;
+        //fetchData(bounds);
+//                ne = bounds.northeast;
+//                sw = bounds.southwest;
+        //new DownloadJSON().execute();
+        Log.e(TAG, "bounds1=> "+bounds1);
+        Polygon polygon2 = map.addPolygon(new PolygonOptions()
+                .clickable(true)
+                .add(
+                        bounds1.southwest,
+                        bounds1.northeast
+                ));
+        polygon2.setTag("alpha");
+        stylePolygon(polygon2);
+        Log.e("LatLngs"," southwest: "+bounds1.southwest +" northeast: "+bounds1.northeast);
+
     }
 
     /**
@@ -299,7 +489,21 @@ public class MainActivity extends AppCompatActivity
 
         // Placing a marker on the touched position
         map.addMarker(markerOptions);
-    }
+
+//        double latitude = location.getLatitude();
+//        double longitude = location.getLongitude();
+
+//        LatLngBounds bounds = this.map.getProjection().getVisibleRegion().latLngBounds;
+//        if(!bounds.contains(latLng)){
+//            CameraPosition cameraPosition = new CameraPosition.Builder()
+//                    .target(new LatLng(23.076412,72.507746))
+//                    .zoom(17)
+//                    .bearing(180)
+//                    .tilt(80)
+//                    .build();
+//            map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        }
+//    }
 
     /**
      * Identify address from latlong
@@ -397,5 +601,94 @@ public class MainActivity extends AppCompatActivity
         } catch (SecurityException e) {
             Log.e("Exception: %s", e.getMessage());
         }
+    }
+
+    @Override
+    public void onPolygonClick(Polygon polygon) {
+        // Flip the values of the red, green, and blue components of the polygon's color.
+        int color = polygon.getStrokeColor() ^ 0x00ffffff;
+        polygon.setStrokeColor(color);
+        color = polygon.getFillColor() ^ 0x00ffffff;
+        polygon.setFillColor(color);
+
+        Toast.makeText(this, "Area type " + polygon.getTag().toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPolylineClick(Polyline polyline) {
+// Flip from solid stroke to dotted stroke pattern.
+        if ((polyline.getPattern() == null) || (!polyline.getPattern().contains(DOT))) {
+            polyline.setPattern(PATTERN_POLYLINE_DOTTED);
+        } else {
+            // The default pattern is a solid stroke.
+            polyline.setPattern(null);
+        }
+
+        Toast.makeText(this, "Route type " + polyline.getTag().toString(),
+                Toast.LENGTH_SHORT).show();
+    }
+
+    private void stylePolyline(Polyline polyline) {
+        String type = "";
+        // Get the data object stored with the polyline.
+        if (polyline.getTag() != null) {
+            type = polyline.getTag().toString();
+        }
+
+        switch (type) {
+            // If no type is given, allow the API to use the default.
+            case "A":
+                // Use a custom bitmap as the cap at the start of the line.
+                polyline.setStartCap(
+                        new CustomCap(
+                                BitmapDescriptorFactory.fromResource(R.drawable.quantum_ic_arrow_back_grey600_24), 10));
+                break;
+            case "B":
+                // Use a round cap at the start of the line.
+                polyline.setStartCap(new RoundCap());
+                break;
+        }
+
+        polyline.setEndCap(new RoundCap());
+        polyline.setWidth(POLYLINE_STROKE_WIDTH_PX);
+        polyline.setColor(COLOR_BLACK_ARGB);
+        polyline.setJointType(JointType.ROUND);
+    }
+
+    /**
+     * Styles the polygon, based on type.
+     * @param polygon The polygon object that needs styling.
+     */
+    private void stylePolygon(Polygon polygon) {
+        String type = "";
+        // Get the data object stored with the polygon.
+        if (polygon.getTag() != null) {
+            type = polygon.getTag().toString();
+        }
+
+        List<PatternItem> pattern = null;
+        int strokeColor = COLOR_BLACK_ARGB;
+        int fillColor = COLOR_WHITE_ARGB;
+
+        switch (type) {
+            // If no type is given, allow the API to use the default.
+            case "alpha":
+                // Apply a stroke pattern to render a dashed line, and define colors.
+                pattern = PATTERN_POLYGON_ALPHA;
+                strokeColor = COLOR_GREEN_ARGB;
+                fillColor = COLOR_PURPLE_ARGB;
+                break;
+            case "beta":
+                // Apply a stroke pattern to render a line of dots and dashes, and define colors.
+                pattern = PATTERN_POLYGON_BETA;
+                strokeColor = COLOR_ORANGE_ARGB;
+                fillColor = COLOR_BLUE_ARGB;
+                break;
+        }
+
+        polygon.setStrokePattern(pattern);
+        polygon.setStrokeWidth(POLYGON_STROKE_WIDTH_PX);
+        polygon.setStrokeColor(strokeColor);
+        polygon.setFillColor(fillColor);
     }
 }
